5e2840caff6bdca17d6dbbc93b0a2801e2afb03e
xww <2775295957@qq.com>
2025-01-11 17:18:24 +0800

    v0.22 Debug信息升级 碰撞检测支持物理碰撞

--- src/main/java/com/xww/Engine/setting/DebugSetting.java
+++ src/main/java/com/xww/Engine/setting/DebugSetting.java
@@ -1,8 +1,25 @@
 package com.xww.Engine.setting;
 
+import com.xww.Engine.core.Component.Component;
+import com.xww.Engine.core.Vector.Vector;
+
 import java.awt.*;
 
 public class DebugSetting {
-    public static boolean IS_DEBUG_ON = false;
+    public static boolean IS_DEBUG_ON = true;
     public static Color DebugInfoColor = Color.RED;
+
+    public static void ShowDebugInfo(Component component, Graphics g){
+        if (IS_DEBUG_ON) {
+            Vector drawPosition = component.getDrawPosition();
+            g.setColor(DebugInfoColor);
+            g.drawString("size: " + component.getSize().toString(), drawPosition.getX(), drawPosition.getY() + 10);
+            g.drawString("position: " + component.getWorldPosition().toString(), drawPosition.getX(), drawPosition.getY() + 20);
+            g.drawString("velocity: " + component.getVelocity().toString(), drawPosition.getX(), drawPosition.getY() + 30);
+            g.drawString("acceleration: " + component.getAcceleration().toString(), drawPosition.getX(), drawPosition.getY() + 40);
+            g.drawString("order: " + component.getOrder(), drawPosition.getX(), drawPosition.getY() + 50);
+            g.drawString("collisionRegion: " + component.getCollisionRegion(), drawPosition.getX(), drawPosition.getY() + 60);
+            g.drawString("alive: " + component.isAlive(), drawPosition.getX(), drawPosition.getY() + 70);
+        }
+    }
 }
